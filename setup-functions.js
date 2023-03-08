f48a8c6951c086659ba2ad2cb97cf4d965974094
Ondym <ondramat007@gmail.com>
2023-03-08 17:02:07 +0100

    the changes i did at school

---
+++ setup-functions.js
@@ -0,0 +1,103 @@
+function createLabel(forAttribute, text) {
+  let label = document.createElement('label');
+  label.setAttribute('for', forAttribute);
+  label.textContent = text;
+  return label;
+}
+
+function createInput(type, id, name, placeholder, required) {
+  let input = document.createElement('input');
+  input.setAttribute('type', type);
+  input.setAttribute('id', id);
+  input.setAttribute('name', name);
+  input.setAttribute('placeholder', placeholder);
+  if (required) {
+    input.setAttribute('required', true);
+  }
+  return input;
+}
+
+function createSelect(id, name, required, options) {
+  let select = document.createElement('select');
+  select.setAttribute('id', id);
+  select.setAttribute('name', name);
+  if (required) {
+    select.setAttribute('required', true);
+  }
+  options.forEach(option => {
+    let optionElement = document.createElement('option');
+    optionElement.setAttribute('value', option.value);
+    optionElement.textContent = option.text;
+    if (option.disabled) {
+      optionElement.setAttribute('disabled', true);
+    }
+    if (option.selected) {
+      optionElement.setAttribute('selected', true);
+    }
+    select.appendChild(optionElement);
+  });
+  return select;
+}
+
+function createRadios(radioParams) { //, id, name, value, text, required
+  let radios = new Array;
+  radioParams.forEach(r => {
+    let radioWrapper = document.createElement('label');
+    let radio = document.createElement('input');
+    radio.setAttribute('type', 'radio');
+    radio.setAttribute('id', r.id);
+    radio.setAttribute('name', r.name);
+    radio.setAttribute('value', r.value);
+    if (r.required) {
+      radio.setAttribute('required', true);
+    }
+    radioWrapper.appendChild(radio);
+    let radioText = document.createTextNode(r.text);
+    radioWrapper.appendChild(radioText);
+
+    radios.push(radioWrapper);
+  });
+  return radios;
+}
+
+function createCheckbox(id, name, value, label) {
+  const checkbox = document.createElement('input');
+  checkbox.setAttribute('type', 'checkbox');
+  checkbox.setAttribute('id', id);
+  checkbox.setAttribute('name', name);
+  checkbox.setAttribute('value', value);
+
+  const checkboxLabel = document.createElement('label');
+  checkboxLabel.setAttribute('for', id);
+  checkboxLabel.textContent = label;
+
+  const container = document.createElement('div');
+  container.appendChild(checkbox);
+  container.appendChild(checkboxLabel);
+
+  return container;
+}
+function createRadio(id, name, value, text, required) {
+  let radioWrapper = document.createElement('label');
+  let radio = document.createElement('input');
+  radio.setAttribute('type', 'radio');
+  radio.setAttribute('id', id);
+  radio.setAttribute('name', name);
+  radio.setAttribute('value', value);
+  if (required) {
+    radio.setAttribute('required', true);
+  }
+  radioWrapper.appendChild(radio);
+  let radioText = document.createTextNode(text);
+  radioWrapper.appendChild(radioText);
+  return radioWrapper;
+}
+function margeRadios(parent, radios) {
+  radios.forEach(function(radio) {
+    parent.appendChild(radio);
+  });
+}
+
+function createOption(id, name, value, text) {
+  return {id, name, value, text};
+}
---
+++ setup.js
@@ -0,0 +1,66 @@
+// let formWrapper = document.querySelector('.form-wrapper');
+let form = document.createElement('form');
+form.setAttribute('method', 'GET');
+form.setAttribute('action', 'form.php');
+
+// ABOUT USER
+
+let ageLabel = createLabel('age', 'Věk:');
+let ageInput = createInput('number', 'age', 'age', 'Zadejte svůj věk', true);
+ageInput.setAttribute("max", 99);
+ageInput.setAttribute("min", 5);
+form.appendChild(ageLabel);
+form.appendChild(ageInput);
+
+let genderLabel = createLabel('gender', 'Pohlaví:');
+let genderSelect = createSelect('gender', 'gender', true, [
+    { value: '', text: 'Vyberte své pohlaví', disabled: true, selected: true },
+    { value: 'male', text: 'Muž' },
+    { value: 'female', text: 'Žena' },
+]);
+form.appendChild(genderLabel);
+form.appendChild(genderSelect);
+
+let playGamesLabel = createLabel('play-games', 'Hrajete videohry?');
+let yesRadio = createRadio('yes', 'play_games', 'yes', 'Ano', true);
+let noRadio = createRadio('no', 'play_games', 'no', 'Ne');
+playGamesLabel.appendChild(yesRadio);
+playGamesLabel.appendChild(noRadio);
+form.appendChild(playGamesLabel);
+
+// ABOUT VIDEOGAMES
+
+let gamesForm = document.createElement('div');
+gamesForm.id = "gamesPart";
+
+form.appendChild(document.createElement("span"))
+
+let howOftenLabel = createLabel("how-often", "Jak často hrajete videohry?");
+[
+    createOption("more-than-two",        "how-often", 1, "Více než 2h denně"),
+    createOption("less-than-two",        "how-often", 2, "Každý den, méně než 2h"),
+    createOption("every-week",           "how-often", 3, "Každý týden"),
+    createOption("every-month",          "how-often", 4, "Párkrát za měsíc"),
+    createOption("less-than-every-month","how-often", 5, "Méně než párkrát za měsíc")
+].forEach(function (option) {
+    howOftenLabel.appendChild(createRadio(option.id, option.name, option.value, option.text));
+});
+gamesForm.appendChild(howOftenLabel);
+
+form.appendChild(gamesForm);
+
+let submitButton = createInput('submit', 'submit', 'submit', 'Přejít k testu reflexů', false);
+form.appendChild(submitButton);
+
+function postSetup() {
+    document.getElementsByName("play_games").forEach(radio => {
+        let gamesForm = document.getElementById("gamesPart");
+        radio.addEventListener("change", function (e) {
+            if (e.target.id == "yes") {
+                gamesForm.className = "";
+            } else {
+                gamesForm.className = "hide";
+            }
+        });
+    });
+}
