b33ab97298b9f3dbbec5e46ef79a3071703792f2
Ondym <77937293+Ondym@users.noreply.github.com>
2024-06-17 23:25:48 +0200

    complete rest of the page, change of use

--- script.js
+++ script.js
@@ -1,60 +1,146 @@
-let timeZero = 0;
-let time = new Date;
+let nodes;
+let nodeSize;
+let totalBlobs;
+let lastBlobNodes;
+let n;
+let HUEstep;
 
-let result = new Array;
-let testInProgress = false;
-let block;
-let lightUp;
+Object.defineProperty(Array.prototype, 'includesVector', {
+    value: function(_v) { 
+        for (let i = 0; i < this.length; i++) {
+            if (this[i].x == _v.x && this[i].y == _v.y) {
+                return true;
+            }
+        }
 
-function beginTest() {
-    if (testInProgress) { return; }
-    block = document.getElementById('clickable-block');
-    testInProgress = true;
-    lightUp = false;
+        return false;
+     }
+});
 
-    document.getElementById("test-explaining").style.display = "none";
-    setTimeout(() => {
-        block.style.backgroundColor = "#af5059";
-        lightUp = true;
-        stopWatch(true);
-    }, (Math.random() * 3000) + 1000);
+function setup() {
+    createCanvas(700, 700);
+
+    nodes = new Array;
+    let size = 100;
+    n = 0;
+    for (let i = 0; i < size; i++) {
+        nodes.push(new Array);
+        for (let j = 0; j < size; j++) {
+            nodes[i].push(genValue(0.5));
+            if (nodes[i][j]) n++;
+        }
+    }
+
+    nodeSize = width / nodes.length;
+    totalBlobs = separateBlobs();
+    HUEstep = 255 / totalBlobs.length;
+
+    lastBlobNodes = new Array;
+
+    let avgNodesPerBlob = floor(n / totalBlobs.length * 10**4) / 10**4;
+
+    let info = document.getElementById("tit");
+    info.innerText = "Počet (černých) bodů: " + n + "\nPočet blobů: " + totalBlobs.length + "\nAVG # bodů v blobu: " + avgNodesPerBlob;
 }
 
-let cheated = false;
-function blockClicked() {
-    if (!lightUp) {
-        cheated = true;
-        return;
+function draw() {
+    noStroke();
+    background(255);
+    fill(0);
+
+    for (let i = 0; i < nodes.length; i++) {
+        for (let j = 0; j < nodes[0].length; j++) {
+            if (nodes[i][j]) {
+                rect(i*nodeSize, j*nodeSize, nodeSize, nodeSize);
+            }
+        }
     }
-    if (cheated) {
-        result.push(-1);
-    } else {
-        result.push(stopWatch());
-    } 
+
     
-    block.style.background = "#000";
-    lightUp = false;
-    cheated = false;
-    
-    if (result.length < 5) {
-        setTimeout(() => {cheated = false}, 700)
-        setTimeout(() => {
-            block.style.backgroundColor = "#af5059";
-            lightUp = true;
-            stopWatch(true);
-        }, Math.floor(Math.random() * 4000) + 2000);
-    } else {
-        result = String(result);
-        resultInput.value = result;
-        document.getElementById("submit").click();
+    // console.log(HUEstep);
+    colorMode(HSB);
+    for (let i = 0; i < totalBlobs.length; i++) {
+        for (let j = 0; j < totalBlobs[i].length; j++) {
+            fill(i * HUEstep, 90, 90);
+            rect(totalBlobs[i][j].x * nodeSize, totalBlobs[i][j].y * nodeSize, nodeSize, nodeSize);
+        }
+    }
+    colorMode(RGB);
+
+    fill(10);
+    for (let i = 0; i < lastBlobNodes.length; i++) {
+        rect((lastBlobNodes[i].x + .25) * nodeSize, (lastBlobNodes[i].y + .25) * nodeSize, nodeSize/2, nodeSize/2);
     }
 }
 
-function stopWatch(_set) {
-    time = new Date;
-    if (_set) {
-        timeZero = time.getTime();
-        return;
+function mouseClicked() {
+    lastBlobNodes = getBlob(floor(mouseX/nodeSize), floor(mouseY/nodeSize));
+    // console.log(lastBlobNodes);
+}
+
+function genValue(_p) {
+    // _p is the probability of getting 1
+    return random() < _p ? 1 : 0
+}
+
+function getBlob(_x, _y) {
+    if (!nodes[_x][_y]) return new Array;
+    let opened = [simpleVector(_x, _y)];
+    let blobNodes = [].concat(opened);
+
+    let m = 0;
+    while (opened.length > 0 && m < 100) {
+        // break;
+        m++;
+        let newOpened = new Array;
+
+        for (let i = 0; i < opened.length; i++) {
+            for (let X = -1; X < 2; X++) {
+                for (let Y = -1; Y < 2; Y++) {
+                    // console.log(X, Y);
+                    if (abs(X) + abs(Y) == 1) {
+                        let nextVector = simpleVector(
+                            (opened[i].x + X + nodes.length) % nodes.length, // adding nodes.length to wrap -1 to the other side
+                            (opened[i].y + Y + nodes.length) % nodes.length
+                        );
+                        if (!blobNodes.includesVector(nextVector) 
+                            && nodes[nextVector.x][nextVector.y]) {
+                            newOpened.push(nextVector);
+                            // console.log(newOpened);
+                            blobNodes.push(nextVector);
+                        }
+                    }
+                }
+            }
+        }
+
+        opened = newOpened;
+        // console.log(m, opened);
     }
-    return Math.abs(timeZero - time.getTime());
+
+    return blobNodes;
+}
+
+function simpleVector(_x, _y) {
+    return {x: _x, y: _y};
+}
+
+function separateBlobs() {
+    let nodesRegistered = [ ];
+    let blobs = new Array;
+    for (let i = 0; i < nodes.length; i++) {
+        for (let j = 0; j < nodes.length; j++) {
+            if (!nodesRegistered.includesVector(simpleVector(i, j))) {
+                let blob = getBlob(i, j);
+                if (blob.length > 0) {
+                    nodesRegistered = nodesRegistered.concat(blob);
+                    blobs.push(blob);
+                }
+            }
+        }
+    }
+
+    console.log(nodesRegistered);
+
+    return blobs;
 }
